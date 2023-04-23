6a8b644643c75680bb12138a53f95039440121d6
lnwanghao <872888760@qq.com>
2023-04-23 14:11:24 +0800

    initial commit

---
+++ packages/pkg1/dist/index.js
@@ -0,0 +1,16 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global["monorepo-base-project"] = {}));
+})(this, (function (exports) { 'use strict';
+
+  function pkg1() {
+      console.log("I am pkg1");
+  }
+
+  exports.pkg1 = pkg1;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
+//# sourceMappingURL=index.js.map
---
+++ packages/pkg1/src/index.ts
@@ -0,0 +1,3 @@
+export function pkg1(): any {
+  console.log("I am pkg1");
+}
