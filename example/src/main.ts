d787c68ed02edc3c10254624a9baaddcdb253fc9
lnwanghao <872888760@qq.com>
2023-05-10 16:22:26 +0800

    test: 修改测试代码

--- example/src/main.ts
+++ example/src/main.ts
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import './assets/main.css';
+import WebPerformance from '@whjs/performance';
+
+const webPerformace = new WebPerformance();
+console.log(webPerformace);
+webPerformace.getPerformanceDetail((...params) => {
+  console.log(...params);
+});
 
 new Vue({
   render: (h) => h(App),
